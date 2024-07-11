72f9db0b1cc65820cb1129591371946db49016bc
Gonzalo Villalba <144571299+gonvlla@users.noreply.github.com>
2024-07-10 22:45:42 -0300

    Serializacion-Pickle.py

--- Serializacion-Pickle.py
+++ Serializacion-Pickle.py
@@ -1,6 +1,6 @@
 import pickle
 listaNombres = ["Gon","Tito","Xiomara","Maia"]
-archivo_binario = open("archivo1","wb")
+archivo_binario = open("archivo1","wb") #Escribira en binario
 pickle.dump(listaNombres,archivo_binario)
 archivo_binario.close()
 del archivo_binario
