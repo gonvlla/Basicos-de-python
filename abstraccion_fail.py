b14ef5ced39c095e7fe5268e8f8cdeb97b6d8d6b
Gonzalo Villalba <144571299+gonvlla@users.noreply.github.com>
2024-07-10 22:46:49 -0300

    abstraccion_fail.py

---
+++ abstraccion_fail.py
@@ -0,0 +1,23 @@
+from abc import ABC,abstractmethod
+class myInterface(ABC):
+    @abstractmethod_ 
+    def metodo1(self):
+        pass
+    @abstractmethod_ 
+    def metodo2(self):
+        pass
+    
+    
+class classA():
+    def method1(self):
+        print("Class A-Method 1")
+    def method2(self):
+        print("Class A-Method 2")
+    
+class classB():
+    def method2(self);
+    print("Class B-Method 1")
+ def method2(self):
+        print("Class B-Method 2")
+objA = classA()
+objB = classB()
